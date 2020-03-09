c4548db32d995cec7564c6f306da4cac15039b33
cho881020 <kj_cho@nepp.kr>
2020-03-09 15:17:58 +0900

    로또 등수 판단 - 2등 제외
     - 당첨번호 / 내 번호 중 같은 갯수를 판단
     - 그 갯수에 따라 다른 결과 출력

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -64,6 +64,59 @@ public class MainDrive {
 			System.out.println(num);
 		}
 		
+//		내가 입력한번호가 몇등인지 비교.
+		
+//		이번주 당첨번호를 배열로. 5, 18, 20, 23, 30, 34
+		
+		int[] thisWeekLotto = {5, 18, 20, 23, 30, 34};
+		
+//		몇등인지 판단.
+//		1등? 내가 입력한 숫자와 / 당첨번호의 숫자중 같은 갯수가 6개면.
+//		2등? 차후에
+//		3등? 같은 갯수가 5개면.
+//		4등? 4개
+//		5등? 3개
+//		그 이하면? 꽝
+		
+//		몇개를 맞췄는지 저장할 변수.
+		int correctCount = 0;
+		
+//		내 로또 번호를 하나씩 꺼내보는 for
+		for (int i=0 ; i < userLottoArr.length ; i++) {
+			
+//			당첨번호 하나씩 꺼내보는 for
+			for (int j=0 ; j < thisWeekLotto.length ; j++) {
+				
+//				내 번호 / 당첨번호 비교. 같은가? 
+				if (userLottoArr[i] == thisWeekLotto[j]) {
+//					같다면? "맞춘 갯수"가 하나 증가.
+					correctCount++;
+				}
+				
+			}
+			
+			
+		}
+		
+		
+		if (correctCount == 6) {
+			System.out.println("1등!");
+		}
+		else if (correctCount == 5) {
+			System.out.println("3등!");
+		}
+		else if (correctCount == 4) {
+			System.out.println("4등!");
+		}
+		else if (correctCount == 3) {
+			System.out.println("5등!");
+		}
+		else {
+			System.out.println("꽝 ㅜㅠ");
+		}
+		
+		
+		
 		
 	}
 	
